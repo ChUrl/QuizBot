dbc65cf606f7b1ec0b4882561a2f656adaf4d032
churl <tobi@urpost.de>
2022-01-21 00:28:03 +0100

    update launch script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d --env-file .env --mount src=/root/QuizBot/quizes,target=/quiz,type=bind registry.gitlab.com/churl/quizbot
+docker run -d --env-file .env --mount src=/home/christoph/QuizBot/quizes,target=/quiz,type=bind registry.gitlab.com/churl/quizbot
