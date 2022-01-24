0cd525b35a889cd30e87ede869e2418835cba62b
churl <tobi@urpost.de>
2022-01-24 22:04:46 +0100

    launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git pull
+
 docker pull registry.gitlab.com/churl/quizbot
 docker container rm -f quizbot
 docker run -d --env-file /home/christoph/QuizBot/.env --mount src=/home/christoph/QuizBot/quizes,target=/quiz,type=bind --name quizbot registry.gitlab.com/churl/quizbot
